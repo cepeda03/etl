fb53b1bd2db4f9017789a964d8404863d2714c4a
Miguel Cepeda <miguelcepeda257@gmail.com>
2026-04-13 18:03:50 -0400

    cc

--- etl.py
+++ etl.py
@@ -1,12 +1,15 @@
 import mysql.connector
 import pandas as pd
 
-conexion = mysql.connector.connect(
-    host = "10.50.48.227",
-    user = "dw_user",
-    password = "123456",
-    database = "dw_star_lab"
-)
+
+
+conexion = create_engine("mysql")
+#conexion = mysql.connector.connect(
+    #host = "10.50.48.227",
+    #user = "dw_user",
+    #password = "123456",
+    #database = "dw_star_lab"
+#)
 
 query = """
 
@@ -19,3 +22,16 @@ df = pd.read_sql(query, conexion)
 
 print("Datos extraidos: ")
 print(df.head())
+
+df = df.dropma()
+
+df["precio unitario"] = df["total"] / df["cantidad"]
+
+df["fecha"] = pd.to_datetime{df("fecha")}
+
+df["mes"] = df["fecha"].dt.month
+
+ventas_por_ciudad = df.groupby("ciudad")["total"].sum().reset_index()
+
+print("ventas por ciudad", ventas_por_ciudad)
+
